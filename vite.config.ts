3bdd6a237b428cf6f6e9abdbcba2a445dae740ff
Wiwik Ismiati <wiwikismiati61@guru.smp.belajar.id>
2026-02-22 14:18:18 +0700

    build: Configure Vercel and Vite for deployment
    Removes Vercel-specific build script from `package.json` and `vercel.json` as it's no longer needed.
    Sets Vite's `base` option to './' for correct routing in Vercel deployments.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    base: './',
     build: {
       outDir: 'dist',
     },
